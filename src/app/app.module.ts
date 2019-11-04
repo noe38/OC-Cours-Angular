32ce12261ed7ba8b561508cfca79c966156359c1
noemie graignic <noemie.graignic@free.fr>
2019-11-04 15:35:56 +0100

    Création d'un guard: il faut s'authentifier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,15 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourHoFourComponent } from './four-ho-four/four-ho-four.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 const appRoutes: Routes = [
-  { path: 'appareils', component: AppareilViewComponent },
-  { path: 'appareils/:id', component: SingleAppareilComponent},
+  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
   { path: 'auth', component: AuthComponent },
   { path: '', component: AppareilViewComponent },
-  { path: 'not-found', component: FourHoFourComponent},
-  { path: '**', redirectTo:'/not-found'}
+  { path: 'not-found', component: FourHoFourComponent },
+  { path: '**', redirectTo: '/not-found' }
 ]
 
 @NgModule({
@@ -40,7 +41,8 @@ const appRoutes: Routes = [
   ],
   providers: [
     AppareilService,
-    AuthService],
+    AuthService,
+    AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
