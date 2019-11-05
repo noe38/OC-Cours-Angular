b590e7b4beba92de60eba654dc65ed41fe42f2b4
noemie graignic <noemie.graignic@free.fr>
2019-11-05 12:29:45 +0100

    Création d'un observable: compteur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  secondes: number;
+
   constructor(){}
+
+  ngOnInit(){
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      (error: any) =>{
+        console.log('Une erreur a été rencontrée !')
+      },
+      ()=> {
+        console.log('Observable complétée !');
+      }
+      );
+  }
 }
